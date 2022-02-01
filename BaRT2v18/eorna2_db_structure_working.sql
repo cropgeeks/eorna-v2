e51756726b88d27a11a5974ce73f2f168130c05d
LindaMilne <33484959+LindaMilne@users.noreply.github.com>
2022-02-01 14:24:08 +0000

    Added gene_annotation table. Altered field sizes in fasta_to_sql.pl and gtf_to_sql.pl because some transcript_ids are longer.

--- BaRT2v18/eorna2_db_structure_working.sql
+++ BaRT2v18/eorna2_db_structure_working.sql
@@ -10,7 +10,7 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `transcript_sequences`;
 CREATE TABLE `transcript_sequences`  (
-  `transcript_id` varchar(25) PRIMARY KEY,
+  `transcript_id` varchar(30) PRIMARY KEY,
   `gene_id` varchar(20),
   `dataset_name` varchar(10),
   `transcript_sequence` longtext,
@@ -26,19 +26,40 @@ CREATE TABLE `transcript_sequences`  (
 -- ----------------------------
 -- Table structure for transcript_structure
 -- ----------------------------
-DROP TABLE IF EXISTS `transcript_structure`;
-CREATE TABLE `transcript_structure`  (
-  `transcript_id` varchar(20),
-  `dataset_name` varchar(30),
-  `f_start` int(7),
-  `f_end` int(7),
-  `gene_id` varchar(20),
-  `chr_id` varchar(20),
+DROP TABLE IF EXISTS `transcript_structures`;
+CREATE TABLE `transcript_structures`  (
+  `transcript_id` varchar(65),
+  `dataset_name` varchar(10),
+  `f_start` int(8),
+  `f_end` int(8),
+  `gene_id` varchar(65),
+  `chr_id` varchar(5),
   `strand` varchar(1),
   `exon_number` int(4),
 
 INDEX `transcript_dataset`(`transcript_id`, `dataset_name`) USING BTREE
 );
+
+-- ----------------------------
+-- Table structure for gene_annotation
+-- ----------------------------
+DROP TABLE IF EXISTS `gene_annotation`;
+CREATE TABLE `gene_annotation`  (
+  `gene_id` varchar(65),
+  `dataset_name` varchar(10),
+  `chr_id` varchar(5)L,
+  `number_of_transcripts`,
+  `gene_start` int(8),
+  `gene_end` int(8),
+  `strand` varchar(1),
+  `pannzer_annotation`,
+  `go_ids` text,
+  `go_terms` text,
+  PRIMARY KEY (`gene_id`) USING BTREE,
+  INDEX `transcript_dataset`(`dataset_name`) USING BTREE,
+  INDEX `gene_dataset`(`gene_id`, `dataset_name`) USING BTREE,
+  INDEX `chr_dataset`(`chr_id`, `dataset_name`) USING BTREE
+) ROW_FORMAT = Dynamic;
 
 
 
