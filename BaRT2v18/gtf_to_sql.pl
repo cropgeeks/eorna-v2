9494386154d47f98cc5b653d70183b5b6c989e07
LindaMilne <33484959+LindaMilne@users.noreply.github.com>
2022-02-01 14:22:07 +0000

    Altered the name of the "transcript_structure" table to "transcript_structures" and changed the gtf_to_sql.pl script accordingly.

--- BaRT2v18/gtf_to_sql.pl
+++ BaRT2v18/gtf_to_sql.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/perl
 
+# Converts a gtf file into sql for adding to the transcript_structure tables in eorna2
+
 use strict;
 
 print "use eorna2;\n";
@@ -17,28 +19,24 @@ while(<GTF>){
 
     if ($feature eq "exon"){
 
+      my ($transcript_id, $gene_id) = split(/\"\; /, $attribute, 2);
     
-    my ($transcript_id, $gene_id) = split(/\"\; /, $attribute, 2);
-    
-    $gene_id =~ s/gene_id \"//;
-    $gene_id =~ s/\"\;//;
-    $transcript_id =~ s/transcript_id \"//;
+      $gene_id =~ s/gene_id \"//;
+      $gene_id =~ s/\"\;//;
+      $transcript_id =~ s/transcript_id \"//;
 
-    
-    if($transcript_id ne $prev_transcript_id){
-      $exon_number = 0;
+      if($transcript_id ne $prev_transcript_id){
+        $exon_number = 0;
+      }
+
+      $exon_number++;
+
+      print "insert into transcript_structures(transcript_id, dataset_name, f_start, f_end, gene_id, chr_id, strand, exon_number) values 
+      (\"$transcript_id\", \"BaRT2v18\", \"$start\", \"$end\", \"$gene_id\", \"$chr\", \"$strand\", \"$exon_number\");\n";
+
+      $prev_transcript_id = $transcript_id;
+
+
     }
-
-    $exon_number++;
-
-    print "insert into transcript_structure(transcript_id, dataset_name, f_start, f_end, gene_id, chr_id, strand, exon_number) values 
-    (\"$transcript_id\", \"BaRT2v18\", \"$start\", \"$end\", \"$gene_id\", \"$chr\", \"$strand\", \"$exon_number\");\n";
-
-    $prev_transcript_id = $transcript_id;
-
-  }
-
-
-
 
 }
